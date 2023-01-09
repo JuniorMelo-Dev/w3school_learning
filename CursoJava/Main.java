2c9621947676fcb2da9d323bb7f6c8d90f85201e
JuniorMelo <juniormelo.dev@gmail.com>
2023-01-09 14:15:55 -0300

    Add pasta com CursoJava

---
+++ CursoJava/Main.java
@@ -0,0 +1,9 @@
+// Código de criação e impressão na tela usando 'println'
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("I am learning Java.");
+		System.out.println("It is awesome!");
+	}
+}
