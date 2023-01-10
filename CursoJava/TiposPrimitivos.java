4e640abab90c00f8ef5f313776a7f785d97e211d
JuniorMelo <juniormelo.dev@gmail.com>
2023-01-10 14:22:52 -0300

    Add aulas de Tipos Dados

---
+++ CursoJava/TiposPrimitivos.java
@@ -0,0 +1,18 @@
+// Tipos de Dados Primitivos - byte, short, int, long, float, double, boolean e char
+// Tipos de Dados não-Primitivos - String, Arrays e Classes
+
+public class TiposPrimitivos {
+	public static void main(String[] args) {
+		int meuNum = 9;
+		float myFloatNum = 8.99f;
+		char myLetter = 'J';
+		boolean myBool = false;
+		String myText = "Hello Dev!";
+
+		System.out.println(meuNum);
+		System.out.println(myFloatNum);
+		System.out.println(myLetter);
+		System.out.println(myBool);
+		System.out.println(myText);
+	}
+}
