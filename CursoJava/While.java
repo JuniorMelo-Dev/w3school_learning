3458bc6adc2884191ff4ef4576dc3916bcd48da8
JuniorMelo <juniormelo.dev@gmail.com>
2023-01-15 20:43:27 -0300

    Atualizando aula sobre While, inserindo break e continue.

--- CursoJava/While.java
+++ CursoJava/While.java
@@ -10,7 +10,7 @@ public class While {
             System.out.println(i);
             i++;
         }
-
+        System.out.println("");
         /* Do..While
         O Loop Do/While é uma variante do While. Este loop executará o bloco de código uma vez,
         antes de verificar se a condição é verdadeira, então repetirá o loop enquanto a condição for 
@@ -26,5 +26,26 @@ public class While {
             j++;
         }
         while (j < 5);
+        System.out.println("");
+        // break em loops While
+        int k = 0;
+        while (k < 10) {
+            System.out.println(k);
+            k++;
+            if (k == 4) {
+                break;
+            }
+        }
+        System.out.println("");
+        // continue em loops while
+        int l = 0;
+        while (l < 10) {
+            if (l == 4) {
+                l++;
+                continue;
+            }
+            System.out.println(l);
+            l++;
+        }
     }
 }
