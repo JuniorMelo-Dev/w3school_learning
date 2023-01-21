161cfbcb915bb8821309c5865366c76442593aa5
JuniorMelo <juniormelo.dev@gmail.com>
2023-01-21 20:30:22 -0300

    Sobrecarregando um método para ambos os tipos.

--- CursoJava/Metodos.java
+++ CursoJava/Metodos.java
@@ -22,21 +22,22 @@
  * 
  * Ex: 5 (SOBRECARGA DE MÉTODO)
  * Com sobrecarga de método, vários métodos podem ter o mesmo nome com parâmetros diferentes
- * Considere o exemplo a seguir, que possui dois métodos que adicionam números de tipos diferentes:
+ * Em vez de definir dois métodos que devem fazer a mesma coisa, é melhor sobrecarregar um.
+ * No exemplo abaixo, sobrecarregamos o plusMethod método para funcionar para ambos 'int' e 'double':
 */ 
 
 public class Metodos {
-    static int plusMethodInt(int x, int y) {
+    static int plusMethod(int x, int y) {
         return x + y;
     }
 
-    static double plusMethodDouble(double x, double y){
+    static double plusMethod(double x, double y){
         return x + y;
     }
 
     public static void main(String[] args) {
-        int myNum1 = plusMethodInt(8, 5);
-        double myNum2 = plusMethodDouble(4.3, 6.26);
+        int myNum1 = plusMethod(8, 5);
+        double myNum2 = plusMethod(4.3, 6.26);
         System.out.println("int: " + myNum1);
         System.out.println("double: " + myNum2);
     } 
