69e56cdb708467abb3a177c0fd553bb659b402ef
JuniorMelo <juniormelo.dev@gmail.com>
2023-01-25 19:29:21 -0300

    Adicionando aula Métodos de Acesso com ParÂmetros

--- CursoJava/JavaPOO/Main2.java
+++ CursoJava/JavaPOO/Main2.java
@@ -1,26 +1,30 @@
 package JavaPOO;
 
 /*
- * Estático vs. Público
- * exemplo para demonstrar as diferenças entre os métodos static e public
+ * Métodos de Acesso com um Objeto
+ * Ex: Crie um objeto Carro chamado 'myCar'. Chame os métodos fullThrottle() e no objeto execute o 
+ * programa: speed() myCar
  */
 
 public class Main2 {
-    // Método Static
-    static void myStaticMethod() {
-        System.out.println("Static methods can be called without creating objects");
+    //Criando o método aceleraçãoTotal()
+    public void fullThrottle() {
+        System.out.println("Ocarro está indo o mais rápido que pode!");
     }
 
-    // Método Public
-    public void myPublicMethod() {
-        System.out.println("Public methods be called by creating objects");
+    //Criando o método velocidade() e adicionando um parâmetro
+    public void speed(int maxSpeed, String string) {
+        System.out.println("Velocidade máxima é: " + maxSpeed + string);
     }
 
-    // Método Principal (Main2)
+    /**
+     * Dentro de Main2, chame os métodos no objeto myCar
+     * @param args
+     */
+    
     public static void main(String[] args) {
-        myStaticMethod();
-
-        Main2 myObj = new Main2();
-        myObj.myPublicMethod();
+        final Main2 myCar = new Main2();
+        myCar.fullThrottle();
+        myCar.speed(200, "Km");
     }
 }
