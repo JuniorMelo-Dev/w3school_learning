9e3007daa3acbdf8ae0c9d2b9f5dcdd94c3afea0
JuniorMelo <juniormelo.dev@gmail.com>
2023-01-25 20:11:22 -0300

    Construtores com  vários Parâmetros

--- CursoJava/JavaPOO/Construtor.java
+++ CursoJava/JavaPOO/Construtor.java
@@ -4,26 +4,22 @@ package JavaPOO;
  * Construtores Java
  * Um construtor em Java é um 'método especial' usado para inicializar objetos. O construtor é chamado quando um 
  * objeto de uma classe é criado. Ele pode ser usado para definir valores iniciais para atributos de objeto:
+ * 
+ * Parâmetros do Construtor
+ * Os construtores também podem receber parâmetros, que são usados ​​para inicializar atributos.
  */
 
 public class Construtor {
-    int x;
+    int modelYear;
+    String modelName;
 
-    //Criando o construtor para a classe Principal
-    public Construtor() {
-        x = 6; //setando o valor inicial para o atributo da classe
+    public Construtor(int year, String name) {
+        modelYear = year;
+        modelName = name;
     }
 
     public static void main(String[] args) {
-        Construtor myObj = new Construtor(); //Criando um objeto pra classe principal (Isto chamará o construtor)
-        System.out.println(myObj.x); //Retornando o valor de x
+        Construtor myCar = new Construtor(1969, "Mustang");
+        System.out.println(myCar.modelName + " " + myCar.modelYear);
     }
-
-    /*
-     * Observe que o nome do construtor deve corresponder ao nome da classe e não pode ter um tipo de retorno   
-     * (como void).
-     * Observe também que o construtor é chamado quando o objeto é criado.
-     * Todas as classes têm construtores por padrão: se você não criar um construtor de classe, o Java o criará
-     * para você. No entanto, você não poderá definir valores iniciais para atributos de objeto.
-     */
 }
