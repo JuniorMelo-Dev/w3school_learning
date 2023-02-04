47a4f6b55c3ff6f16db2d2fc1abaa8468b2ec2c8
JuniorMelo <juniormelo.dev@gmail.com>
2023-02-04 09:18:14 -0300

    Adicionando aula sobre Herança.

---
+++ CursoJava/JavaPOO/Heranca.java
@@ -0,0 +1,17 @@
+package javapoo;
+
+class Heranca {
+    protected String brand = "Fiat";
+    public void honk() {
+        System.out.println("Tuut, tuut!");
+    }
+}    
+
+class Car extends Heranca {
+    private String modelName = "Fastback";
+    public static void main(String[] args) {
+        Car myCar = new Car();
+        myCar.honk();
+        System.out.println(myCar.brand + " " + myCar.modelName);
+    }
+}
