73414c573a0df1fdd0df07cad8daa8e3bd2a39d1
JuniorMelo <juniormelo.dev@gmail.com>
2023-02-04 08:30:05 -0300

    Adicionando aula sobre encapsulamento

---
+++ CursoJava/JavaPOO/Encapsular.java
@@ -0,0 +1,13 @@
+package JavaPOO;
+
+public class Encapsular {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+}
---
+++ CursoJava/JavaPOO/Encapsular2.java
@@ -0,0 +1,9 @@
+package JavaPOO;
+
+public class Encapsular2 {
+    public static void main(String[] args) {
+        Encapsular myObj = new Encapsular();
+        myObj.setName("Junior");
+        System.out.println(myObj.getName());
+    }
+}
