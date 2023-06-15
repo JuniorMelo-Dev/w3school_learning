cc50407abc800ae7d7c0629f1d46cf454c837560
JuniorMelo-Dev <juniormelo.dev@gmail.com>
2023-06-15 20:12:16 -0300

    Adicionando aula sobre o tipo 'Matrizes Tipada'

---
+++ Typescript/tuplas.js
@@ -0,0 +1,11 @@
+/* MAtrizes Tipadas
+    Uma 'tupla' é uma matriz tipada com um comprimento e tipos predefinidos para cada índice.
+
+    As tuplas são ótimas porque permitem que cada elemento na matriz seja um tipo conhecido de valor.
+
+    Para definir uma tupla, especifique o tipo de cada elemento na matriz:
+*/
+var suaTupla;
+//inicialização correta
+suaTupla = [5, false, "Iniciando tupla corretamente"];
+console.log(suaTupla);
---
+++ Typescript/tuplas.ts
@@ -0,0 +1,16 @@
+/* Matrizes Tipadas 
+    Uma 'tupla' é uma matriz tipada com um comprimento e tipos predefinidos para cada índice.
+
+    As tuplas são ótimas porque permitem que cada elemento na matriz seja um tipo conhecido de valor.
+
+    Para definir uma tupla, especifique o tipo de cada elemento na matriz:
+*/
+
+let suaTupla: [number, boolean, string];
+
+//inicialização correta
+suaTupla = [5, false, "Iniciando tupla corretamente"]
+console.log(suaTupla);
+
+//inicialização incorreta
+//suaTupla = [true, "Iniciando tupla incorretamente", 33];
